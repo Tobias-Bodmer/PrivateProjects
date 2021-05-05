7cff1969501b98a7d21b9f00bfd84febf08ab00d
Tobias Bodmer <48796225+Tobias-Bodmer@users.noreply.github.com>
2021-05-05 22:25:56 +0200

    trying some designs

---
+++ StrategieSpiel/main.ts
@@ -0,0 +1,8 @@
+document.addEventListener("click", doEvent(e), false);
+
+function doEvent(event: Event): void {
+    //switch(event.target){
+    //    case :
+    //}
+    console.log(event.target);
+}
